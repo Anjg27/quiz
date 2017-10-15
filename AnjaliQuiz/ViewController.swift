1918b33b372ff10cf7cce28a0caaacdd7c71a5ce
Anjali <anjalig27@gmail.com>
2017-10-15 12:25:49 -0700

    Create question answer model

--- AnjaliQuiz/ViewController.swift
+++ AnjaliQuiz/ViewController.swift
@@ -17,6 +17,107 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerButton3: UIButton!
     @IBOutlet weak var submitButton:  UIButton!
     
+    let questions            = [
+        "What is the capital of Tajikistan?",
+        "Tbilisi is the capital of what country?",
+        "What is the capital of Mongolia?",
+        "Manlia is the capital of what country?",
+        "What is the capital of Nepal?",
+        "Vientiane is the capital of what country?",
+        "What is the capital of North Korea?",
+        "Colombo is the capital of what country?",
+        "What is the capital of Syria?",
+        "Tashkent is the capital of what country?"
+    ]
+    let answers              = [
+        [
+            [
+                "Jerusalem",
+                "Dushanbe",
+                "Dhaka",
+                "Bangkok"
+            ],
+            [
+                "Armenia",
+                "Turkey",
+                "Russia",
+                "Georgia"
+           ],
+           [
+                "New Delhi",
+                "Amman",
+                "Ulaanbaatar",
+                "Phnom Penh"
+            ],
+            [
+                "Indonesia",
+                "Phillipines",
+                "Qatar",
+                "Bhutan"
+            ],
+            [
+                "Katmandu",
+                "Rangoon",
+                "Damascus",
+                "Jakarta"
+            ],
+            [
+                "Cambodia",
+                "Vietnam",
+                "Laos",
+                "Thailand"
+            ],
+            [
+                "Beijing",
+                "Ashtana",
+                "Pyongyang",
+                "Doha"
+            ],
+            [
+                "Sri Lanka",
+                "India",
+                "Kyrgzstan",
+                "Iraq"
+            ],
+            [
+                "Kabul",
+                "Ankara",
+                "Teran",
+                "Damascus"
+            ],
+            [
+                "Yemen",
+                "Uzbekistan",
+                "Kazakhastan",
+                "Oman"
+            ]
+        ]
+    ]
+    let correctAnswers       = [
+        0,
+        1,
+        1,
+        3,
+        2,
+        2,
+        3,
+        1,
+        4,
+        0,
+        5,
+        2,
+        6,
+        2,
+        7,
+        0,
+        8,
+        3,
+        9,
+        0
+    ]
+    
+    let currentQuestionIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
